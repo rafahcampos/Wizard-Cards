b4c636f48b55400323c3be8e8baa5918bfb3ceca
Rafael Campos <rafael.campos.m@hotmail.com>
2024-06-10 00:10:40 -0300

    Finalização da lógica na criação dos personagens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,20 @@
 //https://hp-api.herokuapp.com/
 const apiCharacters = "https://hp-api.herokuapp.com/api/characters";
 //const apiSpells = ("https://hp-api.herokuapp.com/api/spells");
+let charactersData = [];
+let cardIdCounter = 0; //contador que garante que cada card terá um id único
 
 async function getApiCharacter(api) {
   try {
     //Realizando a requisição para a API
     const response = await fetch(api);
-
     //Verifica o status da requisição
     if (!response.ok) {
       throw new Error(`Erro! Status da resposta: ${response.status}`);
     }
-
     //Convertendo para JSON
     const data = await response.json();
-
     //Printando os dados
-    // console.log(data);
 
     //retorna os dados para uso futuro
     return data;
@@ -27,62 +25,61 @@ async function getApiCharacter(api) {
 }
 
 //Exibir os dados na página HTML
-function displayCharacterDetails(data, characterName) {
-  const containerCharacterDetails =
-    document.querySelector(".character-details");
+function displayCharacterDetails(character) {
+  const containerCharacterDetails = document.querySelector(".character-details");
+  const cardId = `card-${cardIdCounter}`;
 
-  const character = data.find(
-    (item) => item.name.toLowerCase() === characterName.toLowerCase() //verifica se encontrado
-  );
+  console.log(cardId);
+  
+    
+  const cardHtml = `
+    
+    <div class="card-container-character" data-id="${cardId}">
+        <img src="${character.image}" alt="Imagem dos Personagens">
+        <div class="card-container-informations">
+            <div class="character-information-style">
+                <p>Nome: ${character.name}</p>
+                <p>Espécie: ${character.species}</p>
+                <p>Casa: ${character.house}</p>
+                <p>Ancestralidade: ${character.ancestry}</p>
+                <div class="button-submit">
+                    <button type="button" class="edit-button"
+                        data-id="${cardId}">
+                        Alterar Character</button>
+                </div>
+                <div class="button-submit">
+                    <button type="button" class="delete-button"
+                        data-id="${cardId}">
+                        Excluir Character</button>
+                </div>
+            </div>
+        </div>
+    </div>`;
 
-  if (character) {
-    containerCharacterDetails.innerHTML += `
-          <b>
-                    <div class="card-container-character">
-                        <img src="${character.image}"
-                            alt="Imagem dos Personagens">
-                        <div class="card-container-informations">
-                            <b>
-                                <div>
-                                    <p>Nome: ${character.name}</p>
-                                    <p>Espécie: ${character.species}</p>
+    
+    containerCharacterDetails.insertAdjacentHTML('beforeend', cardHtml);
+ 
 
-                                    <p>Casa: ${character.house}</p>
-                                    <p>Ancestralidade: ${character.ancestry}</p>
-                                    <div  class="button-submit" >
-                                        <button type="submit">Alterar
-                                            Character</button>
-                                        </div>
-                                        <div class="button-submit">
-                                        <button  type="submit">Excluir
-                                            Character</button>
-                                    </div>
-                                </div>
-                            </b>
-                        </div>
-                    </div>
-                </b>`;
+    document.querySelector(`.edit-button[data-id="${cardId}"]`).addEventListener('click', openEditModal);
+    document.querySelector(`.delete-button[data-id="${cardId}"]`).addEventListener('click', deleteCharacterCard);
 
-    //Insetir o html na div especifica
-  } else {
-    containerCharacterDetails.innerHTML += `<p>Personagem não encontrado</p>`;
-  }
-}
+    cardIdCounter ++;
+};
 
 function createCharacterCard() {
-  document
-    .getElementById("searchButton")
-    .addEventListener("click", async () => {
-      const characterName = document
-        .getElementById("characterNameInput")
-        .value.trim();
-
+  document.getElementById("searchButton").addEventListener("click", async () => {
+      const characterName = document.getElementById("characterNameInput").value.trim();
       if (characterName) {
-        displayCharacterDetails(charactersData, characterName);
+          const characters = charactersData.filter(item => item.name.toLowerCase() === characterName.toLowerCase());
+          if (characters.length >= 0) {
+              characters.forEach(character => displayCharacterDetails(character));
+          } else {
+              alert("Personagem não encontrado.");
+          }
       } else {
-        alert("Por favor, digite um nome de personagem.");
+          alert("Por favor, digite um nome de personagem.");
       }
-    });
+  });
 }
 
 function setupAutocomplete(inputElement, suggestionsElement, data) {
@@ -91,16 +88,15 @@ function setupAutocomplete(inputElement, suggestionsElement, data) {
     suggestionsElement.innerHTML = "";
 
     if (query) {
-      const filteredSuggestions = data.filter((character) =>
+      const filteredSuggestions = data.filter(character =>
         character.name.toLowerCase().includes(query)
       );
 
-      filteredSuggestions.forEach((character) => {
+      filteredSuggestions.forEach(character => {
         const suggestionElement = document.createElement("div");
         suggestionElement.className = "autocomplete-suggestion";
         suggestionElement.textContent = character.name;
         suggestionsElement.appendChild(suggestionElement);
-
         suggestionElement.addEventListener("click", () => {
           inputElement.value = character.name;
           suggestionsElement.innerHTML = "";
@@ -116,6 +112,84 @@ function setupAutocomplete(inputElement, suggestionsElement, data) {
   });
 }
 
+//------------------------------------------------------------------------//
+//MODAL
+
+function openEditModal(event) {
+  const id = event.target.dataset.id;
+  const card = document.querySelector(`.card-container-character[data-id="${id}"]`);
+  const characterName = card.querySelector('p:nth-child(1)').innerText.split(': ')[1];
+  const character = charactersData.find(item => item.name === characterName);
+
+  if (character) {
+      document.getElementById('editName').value = character.name;
+      document.getElementById('editSpecies').value = character.species;
+      document.getElementById('editHouse').value = character.house;
+      document.getElementById('editAncestry').value = character.ancestry;
+      document.getElementById('editModal').style.display = 'block';
+
+      document.getElementById('editCharacterForm').onsubmit = function (e) {
+          e.preventDefault();
+          saveCharacterChanges(character, id);
+      };
+  }
+}
+
+function saveCharacterChanges(character, id) {
+  character.name = document.getElementById("editName").value;
+  character.species = document.getElementById("editSpecies").value;
+  character.house = document.getElementById("editHouse").value;
+  character.ancestry = document.getElementById("editAncestry").value;
+
+  const card = document.querySelector(`.card-container-character[data-id="${id}"]`);
+
+  card.querySelector(".card-container-informations").innerHTML = `
+   <div class="character-information-style">
+        <p>Nome: ${character.name}</p>
+        <p>Espécie: ${character.species}</p>
+        <p>Casa: ${character.house}</p>
+        <p>Ancestralidade: ${character.ancestry}</p>
+       <div class="button-submit">
+         <button type="button" class="edit-button" data-id="${id}">Alterar Character</button>
+             </div>
+        <div class="button-submit">
+              <button type="button" class="delete-button" data-id="${id}">Excluir Character</button>
+       </div>
+
+    </div>`;
+
+  document.getElementById("editModal").style.display = "none";
+
+  //Após a alteração, redireciona os cliques para os novos botões (acima)
+  card.querySelector('.edit-button').addEventListener('click', openEditModal);
+  card.querySelector('.delete-button').addEventListener('click', deleteCharacterCard);
+}
+
+function deleteCharacterCard(event) {
+  const id = event.target.dataset.id;
+  const card = document.querySelector(`.card-container-character[data-id="${id}"]`);
+  card.remove();
+}
+
+function openAddModal() {
+  document.getElementById("addModal").style.display = "block";
+}
+
+function addNewCharacter(event) {
+  event.preventDefault();
+
+  const newCharacter = {
+    name: document.getElementById("addName").value,
+    species: document.getElementById("addSpecies").value,
+    house: document.getElementById("addHouse").value,
+    ancestry: document.getElementById("addAncestry").value,
+    image: "https://via.placeholder.com/150", //Imagem do placeholder
+  };
+  charactersData.push(newCharacter);
+  displayCharacterDetails(newCharacter);
+  document.getElementById("addModal").style.display = "none";
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
   charactersData = await getApiCharacter(apiCharacters);
   createCharacterCard();
@@ -124,33 +198,23 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.getElementById("autocompleteSuggestions"),
     charactersData
   );
+
+  document.getElementById("addCharacterButton").addEventListener("click", openAddModal);
+  document.getElementById("addCharacterForm").addEventListener("submit", addNewCharacter);
 });
 
-//------------------------------------------------------------------------//
-//MODAL
-
-document.addEventListener("DOMContentLoaded", () => {
-  const modal = document.getElementById("modal");
-
-  const openModalButton = document.getElementById("openModalButton");
-
-  const closeModalButton = modal.querySelector(".close");
-
-  function openModal() {
-    modal.style.display = "block";
-  }
-
-  function closeModal() {
-    modal.style.display = "none";
-  }
-
-  openModalButton.addEventListener("click", openModal);
-
-  closeModalButton.addEventListener("click", closeModal);
-
-  window.addEventListener("click", (event) => {
-    if (event.target == modal) {
-      closeModal();
-    }
+document.querySelectorAll('.close').forEach(button => {
+  button.addEventListener("click", () => {
+    document.getElementById("editModal").style.display = 'none';
+    document.getElementById("addModal").style.display = 'none';
   });
 });
+
+window.onclick = function (event) {
+  if (event.target === document.getElementById("editModal")) {
+    document.getElementById('editModal').style.display = 'none';
+  }
+  if (event.target === document.getElementById("addModal")) {
+    document.getElementById('addModal').style.display = 'none';
+  }
+};
